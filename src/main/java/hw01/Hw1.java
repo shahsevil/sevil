edbf32ac6aceee81fca35baba0e5ce94870c13cc
shahsevil <you@example.com>
2020-02-17 21:24:22 +0400

    Homework 1(new)

--- src/main/java/hw01/Hw1.java
+++ src/main/java/hw01/Hw1.java
@@ -4,34 +4,30 @@ import java.util.Scanner;
 public class Hw1 {
   public static void main(String[] args) {
 
-        System.out.println("Let the game begin!");
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("Enter your name:");
-        String str1=scanner.nextLine();
-        System.out.println("Enter a number:");
+      System.out.println("Let the game begin!");
+      Scanner scanner=new Scanner(System.in);
+      System.out.println("Enter your name:");
+      String str1=scanner.nextLine();
+      System.out.println("Enter a number:");
 
-        int num=scanner.nextInt();
-        int[] arr = new int[1];
-
-        for(int i = 0; i < arr.length; i++) {
-            arr[i] = (int) (Math.random() * 100 + 1);
-        }
+      int num=scanner.nextInt();
+      int rand = (int) (Math.random() * 100 + 1);
 
       while (true) {
-        for (int el: arr) {
-            if(el>num) {
-                System.out.println("Your number is too small. Please, try again.");
-            }
-            else if (el<num){
-                System.out.println("Your number is too big. Please, try again.");
-            }
-            else {
-                String e=String.format("Congratulations, %s!\n", str1);
-                System.out.println(e);
-            }
-        }
-        num=scanner.nextInt();
-        }
+          if(rand>num) {
+              System.out.println("Your number is too small. Please, try again.");
+              num=scanner.nextInt();
+          }
+          else if (rand<num){
+              System.out.println("Your number is too big. Please, try again.");
+              num=scanner.nextInt();
+          }
+          else {
+              String e=String.format("Congratulations, %s!\n", str1);
+              System.out.println(e);
+              break;
+          }
+      }
     }
 }
 
