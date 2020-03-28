04d8b757a0fcb92d879fd38bbe85574b8c4bfae5
shahsevil <you@example.com>
2020-03-29 02:52:56 +0400

    hw13

--- src/main/java/hw13/Collection/CollectionFamilyDao.java
+++ src/main/java/hw13/Collection/CollectionFamilyDao.java
@@ -3,12 +3,17 @@ package hw13.Collection;
 import hw13.DAO.FamilyDao;
 import hw13.Entity.Family;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CollectionFamilyDao implements FamilyDao {
 
     private List<Family> familyList = new ArrayList<>();
+
 
     @Override
     public List<Family> getAllFamilies() {
@@ -52,8 +57,15 @@ public class CollectionFamilyDao implements FamilyDao {
 
     @Override
     public void loadData(List<Family> families) {
-        familyList.addAll(families);
+        File data = new File("C:\\Users\\shaxs\\IdeaProjects\\sevil\\src\\main\\java\\hw13\\myfile.bin");
+        try {
+            FileInputStream fileInputStream = new FileInputStream(data);
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            familyList = (List<Family>) objectInputStream.readObject();
+            objectInputStream.close();
+            fileInputStream.close();
+        } catch (IOException | ClassNotFoundException ex) {
+            System.out.println("Something went wrong");
+        }
     }
-
-
 }
